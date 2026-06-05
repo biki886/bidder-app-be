fa61e3d65b17c45802ccf7f9aa1ca417e52504f7
Biki Haldar <bikihaldar2454@gmail.com>
2026-06-05 21:44:05 +0530

    simple express server create

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// root file (server created)
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// server listen
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  try {
+    console.log("App is running on", port);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+});
