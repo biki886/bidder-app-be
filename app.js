0f6d03b051c3c2a77bd6281a6902f1c6b5a99e5e
Biki Haldar <bikihaldar2454@gmail.com>
2026-06-06 10:21:00 +0530

    db connection done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const sequelize = require("./src/config/dbConnect");
 const app = express();
 
 app.use(express.json());
@@ -10,9 +11,12 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+app.listen(port, async () => {
   try {
     console.log("App is running on", port);
+    //db connection
+    await sequelize.authenticate();
+    console.log('Db Connection has been established successfully.');
   } catch (error) {
     console.log("Error", error.message);
   }
